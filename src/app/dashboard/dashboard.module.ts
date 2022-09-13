b71eb33ffa34f0667e08a02b83ce2f5ac3667495
ala.mouhamed <ala.mouhamed@tunav.com>
2022-09-13 11:51:27 +0100

    first Commit

---
+++ src/app/dashboard/dashboard.module.ts
@@ -0,0 +1,34 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from "@angular/common";
+
+import { DashboardRoutingModule } from "./dashboard-routing.module";
+import { ChartistModule } from 'ng-chartist';
+import { NgApexchartsModule } from 'ng-apexcharts';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
+import { AngularResizedEventModule } from 'angular-resize-event';
+import { MatchHeightModule } from "../shared/directives/match-height.directive";
+
+import { Dashboard1Component } from "./dashboard1/dashboard1.component";
+import { Dashboard2Component } from "./dashboard2/dashboard2.component";
+import { DashboardComponent } from './dashboard/dashboard.component';
+
+
+@NgModule({
+    imports: [
+        CommonModule,
+        DashboardRoutingModule,
+        ChartistModule,
+        NgbModule,
+        MatchHeightModule,
+        NgApexchartsModule,
+        AngularResizedEventModule
+    ],
+    exports: [],
+    declarations: [
+        Dashboard1Component,
+        Dashboard2Component,
+        DashboardComponent
+    ],
+    providers: [],
+})
+export class DashboardModule { }
