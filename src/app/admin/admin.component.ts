4bc33da64c447c9b5f2975536052239b20e124e1
ala.mouhamed <ala.mouhamed@tunav.com>
2022-09-16 10:40:28 +0100

    add popup in admin to add user form

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { DatatableData } from 'app/data-tables/data/datatables.data';
+
+import {
+    ColumnMode,
+    DatatableComponent,
+    SelectionType
+  } from '@swimlane/ngx-datatable';
+import { AdduserComponent } from './modals/adduser/adduser.component';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-admin',
   templateUrl: './admin.component.html',
-  styleUrls: ['./admin.component.scss']
+  styleUrls: ['./admin.component.scss', '/assets/sass/libs/datatables.scss'],
+  encapsulation: ViewEncapsulation.None
 })
 export class AdminComponent implements OnInit {
+    
+  public rows = DatatableData;
+  public ColumnMode = ColumnMode;
 
-  constructor() { }
+  constructor(private modalService: NgbModal) { }
 
   ngOnInit(): void {
   }
+
+  
+    // Open content with dark section
+    openContent() {
+        const modalRef = this.modalService.open(AdduserComponent);
+        modalRef.componentInstance.name = 'World';
+    }
 
 }
--- src/app/admin/admin.module.ts
+++ src/app/admin/admin.module.ts
@@ -2,14 +2,19 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AdminComponent } from './admin.component';
 import { adminRoutingModule } from './admin-routing.module';
+import { NgxDatatableModule } from '@swimlane/ngx-datatable';
+import { PipeModule } from 'app/shared/pipes/pipe.module';
+import { AdduserComponent } from './modals/adduser/adduser.component';
 
 
 
 @NgModule({
-  declarations: [AdminComponent],
+  declarations: [AdminComponent, AdduserComponent],
   imports: [
     CommonModule,
-    adminRoutingModule
+    NgxDatatableModule,
+    adminRoutingModule,
+    PipeModule
   ]
 })
 export class AdminModule { }
